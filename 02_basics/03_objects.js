20931593b6fd7504b7d6ca6f2e33dc69e6253dc7
Nikhil Mishra <nilhilmishra7315@gmail.com>
2026-03-01 12:25:47 +0000

    studied objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,6 +1,8 @@
 // There are two ways in which an object can be declared
 // 1. Constructor(Singleton Object is formed) | 2. Literals
 
+// In this we declared the objects with the help of literals
+
 const mySym = Symbol("key1")
 
 const JsUser = {
@@ -20,16 +22,31 @@ const JsUser = {
 // console.log(JsUser.email)    //way2
 
 // console.log(JsUser["full name"])
-console.log(JsUser.mySym) //This gives the value but is not the right way of accessing the symbol as its data type is coming as a string
-console.log(typeof JsUser.mySym); //you can check it is coming string here
+// console.log(JsUser.mySym) //This gives the value but is not the right way of accessing the symbol as its data type is coming as a string
+// console.log(typeof JsUser.mySym); //you can check it is coming string here
 
 
-console.log(JsUser[mySym]) // This is the correct way of accessing the symbol inside an onject - this give symbol type
-console.log(typeof JsUser[mySym])
+// console.log(JsUser[mySym]) // This is the correct way of accessing the symbol inside an onject - this give symbol type
+// console.log(typeof JsUser[mySym])
 
 JsUser.email = "nikhil@nvidia"
-console.log(JsUser["email"]) 
+// console.log(JsUser.email) 
 
-Object.freeze(JsUser.email)
+// Object.freeze(JsUser) //no changes will propogate after it
 JsUser.email = "nikhil@mckinsey"
-console.log(JsUser)
+// console.log(JsUser)
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this["full name"]}`);
+    
+}
+console.log(JsUser.greeting);
+console.log(JsUser.greeting());
+
+console.log(JsUser.greetingTwo);
+console.log(JsUser.greetingTwo());
---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,7 @@
+// In this we'll declare the objects with help of constructor(singleton)
+
+const cricBuzzuser = new Object()
+
+const cricBuzzuser.id = ""
+
+console.log(cricBuzzuser);
